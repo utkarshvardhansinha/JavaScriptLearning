1213d43263f607e9956d2082e53a8af54934cb64
utkarshvardhansinha <utkarshvardhansinha@gmail.com>
2025-12-16 21:22:18 +0000

    constAndVar

---
+++ Basics/constAndVar.js
@@ -0,0 +1,25 @@
+const roll=124103018
+let name="utkarsh"   // while declaring a string we need to put " "
+var school= "gurukul kurukshetra"
+let balance=45
+
+
+//  roll=124103011111  const value cannot be re-assigned
+
+school= " GURUKUL KURUKSHETRA"
+
+city= "kurukshetra"
+city="changed to patna"
+balance=56
+balance="454545"   
+balance=23
+
+
+
+
+console.log(roll);
+console.log(name);
+console.log(school);
+console.table([roll,name, school,city,balance])  // things can be printed using tables
+
+
