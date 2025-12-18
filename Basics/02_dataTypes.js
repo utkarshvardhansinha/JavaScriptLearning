59482e908086e3005dca2221365faa1e7ee77f0d
utkarshvardhansinha <utkarshvardhansinha@gmail.com>
2025-12-18 06:34:24 +0000

    dataTypes

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+// alert("hello")  --> we are not using browser it is node js  , this syntex is for browser
+
+
+// Data types-->
+// number
+// string
+// bigInt
+// boolean
+// null=> standalone value
+// undefined
+// symbol => unique
+// object
+
+let age=20
+let name='Utkarsh'
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
+
