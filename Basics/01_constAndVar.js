8c2251dba7405015317bb432ea59793172dd1ac7
utkarshvardhansinha <utkarshvardhansinha@gmail.com>
2026-01-14 10:06:16 +0000

    letandVar_Difference

--- Basics/01_constAndVar.js
+++ Basics/01_constAndVar.js
@@ -4,15 +4,29 @@ var school= "gurukul kurukshetra"
 let balance=45
 
 
-//  roll=124103011111  const value cannot be re-assigned
+//In modern JavaScript, the choice between var and let comes down to how strictly you want to control your data. While both create variables, let was introduced in 2015 (ES6) to fix several "weird" behaviors of var.
 
-school= " GURUKUL KURUKSHETRA"
+
+if (true) {
+    var leakingVar = "I escaped!";
+    let trappedLet = "I am stuck in here!";
+}
+
+console.log(leakingVar); // Outputs: "I escaped!"
+// console.log(trappedLet); // ReferenceError: trappedLet is not defined
+
+
+// roll=124103011111  const value cannot be re-assigned
+
+school= "GURUKUL KURUKSHETRA"
 
 city= "kurukshetra"
 city="changed to patna"
 balance=56
-balance="454545"   
+balance="454545"  
+console.log(typeof(balance)) 
 balance=23
+
 
 
 
